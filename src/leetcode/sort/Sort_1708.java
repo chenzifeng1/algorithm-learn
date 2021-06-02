37a9be0fa29e814baedf2facbf798333473d2c8c
chenzifeng1 <704734862@qq.com>
2021-06-02 19:31:23 +0800

    动态规划解决最长子增长序列
    没搞懂

--- src/leetcode/sort/Sort_1708.java
+++ src/leetcode/sort/Sort_1708.java
@@ -1,19 +1,21 @@
 package leetcode.sort;
 
+import java.util.Arrays;
+
 /**
  * @Author: czf
  * @Description: 马戏团人塔
- *
- *有个马戏团正在设计叠罗汉的表演节目，一个人要站在另一人的肩膀上。出于实际和美观的考虑，在上面的人要比下面的人矮一点且轻一点。已知马戏团每个人的身高和体重，请编写代码计算叠罗汉最多能叠几个人。
- *示例：
- *
+ * <p>
+ * 有个马戏团正在设计叠罗汉的表演节目，一个人要站在另一人的肩膀上。出于实际和美观的考虑，在上面的人要比下面的人矮一点且轻一点。已知马戏团每个人的身高和体重，请编写代码计算叠罗汉最多能叠几个人。
+ * 示例：
+ * <p>
  * 输入：height = [65,70,56,75,60,68] weight = [100,150,90,190,95,110]
  * 输出：6
  * 解释：从上往下数，叠罗汉最多能叠 6 层：(56,90), (60,95), (65,100), (68,110), (70,150), (75,190)
  * 提示：height.length == weight.length <= 10000
- *
+ * <p>
  * 思路： 题目核心应该是多条件排序
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/circus-tower-lcci
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -23,23 +25,52 @@ package leetcode.sort;
 public class Sort_1708 {
 
     public static void main(String[] args) {
-        int[] height =new int[] {65,70,56,75,60,68} ;
-        int[] weight =new int[] {100,150,90,190,95,110};
-        int[] height_1 =new int[]{2868,5485,1356,1306,6017,8941,7535,4941,6331,6181};
-        int[] weight_1 = new int[]{5042,3995,7985,1651,5991,7036,9391,428,7561,8594};
+        int[] height = new int[]{65, 70, 56, 75, 60, 68};
+        int[] weight = new int[]{100, 150, 90, 190, 95, 110};
+        int[] height_1 = new int[]{2868, 5485, 1356, 1306, 6017, 8941, 7535, 4941, 6331, 6181};
+        int[] weight_1 = new int[]{5042, 3995, 7985, 1651, 5991, 7036, 9391, 428, 7561, 8594};
 
-        System.out.println(bestSeqAtIndex(height_1,weight_1));
+        System.out.println(bestSeqAtIndex(height_1, weight_1));
 
     }
 
-
     public static int bestSeqAtIndex(int[] height, int[] weight) {
-        quickSort(height, weight,0,height.length-1);
-        int index =0;
-        int num =1;
+        int len = height.length;
+
+        int[][] persons = new int[len][2];
+
+        for (int i = 0; i < len; i++) {
+            persons[i][0] = height[i];
+            persons[i][1] = weight[i];
+        }
+        //先按照身高降序排序，再按照体重降序排序
+        Arrays.sort(persons, (x, y) -> x[0] == y[0] ? y[1] - x[1] : x[0] - y[0]);
+        int[] dp = new int[len];
+        int res =0;
+
+        //这里用动态规划来求最长子增长序列
+        for (int[] person : persons) {
+            // 使用二分查找在 找不到就返回
+            int i = Arrays.binarySearch(dp,0,res,person[1]);
+            if(i<0){
+                 i=-(i+1);
+            }
+            dp[i] = person[1];
+            if(i==res){
+                ++res;
+            }
+        }
+        return res;
+    }
+
+
+    public static int bestSeqAtIndexOld(int[] height, int[] weight) {
+        quickSort(height, weight, 0, height.length - 1);
+        int index = 0;
+        int num = 1;
         // 这里不能这么搞，体重A,B,C,D 有可能是B>A>C>D 按逻辑能叠3层，但是这么搞只能叠两层
         for (int i = 1; i < weight.length; i++) {
-            if(weight[index]<weight[i]&&height[index]!=height[i]){
+            if (weight[index] < weight[i] && height[index] != height[i]) {
                 num++;
                 index = i;
             }
@@ -87,7 +118,7 @@ public class Sort_1708 {
         a[head] = pivot;
         b[head] = pivotB;
         quickSort(a, b, left, head);
-        quickSort(a, b, head+1, right);
+        quickSort(a, b, head + 1, right);
 
     }
 }
